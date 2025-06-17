f85d896a7ee3bef0343585a95fc406863f586895
duc11021102 <kieungocanhduc@gmail.com>
2025-06-17 17:17:21 +0700

    update with logging helper

--- tts-audio.py
+++ tts-audio.py
@@ -2,8 +2,7 @@ from gtts import gTTS
 import langs
 import os, pygame, signal
 import intro
-from lib import exit
-from lib import sound
+from lib import exit, sound, helper
 from model.State import SoundState, ModeState
 from typing import Dict
 
@@ -13,6 +12,9 @@ dict_langs: Dict[str, str] = langs.lang
 current_path: str = os.getcwd()
 playing: SoundState = SoundState.PLAYING
 mode: ModeState = ModeState.DEFAULT_MODE
+
+#setup logger
+logger = helper.setup_logger()
 
 # smooth Ctrl C, avoid error commands as exit
 signal.signal(signal.SIGINT, exit.smooth_exit)
@@ -38,27 +40,27 @@ def command(cmd : str) -> int:
 
     if str(lang) in dict_langs:
         default_lang = str(lang)
-        print(f"Set current language to \033[92m{dict_langs[default_lang]}\033[0m successfully!!!")
+        message = f"Set current language to {dict_langs[default_lang]} successfully!!!"
+        logger.info(message)
     else:
         match cmd:
             case '/c':
+                logger.info("Displayed current configuration")
                 print(f'[+] Current language: \033[92m{default_lang} ({dict_langs[default_lang]})\033[0m')
                 print(f'[+] Current mode: \033[92m{"One .mp3 file" if mode == ModeState.DEFAULT_MODE else "Multiple .mp3 files"}\033[0m')
                 print(f'[+] Current sound mode: \033[92m{"On" if playing == SoundState.PLAYING else "Off"}\033[0m')
             case '/m':
                 mode = mode.toggle()
                 if mode == ModeState.MULTIPLE_MODE:
-                    print(f"Set current mode to \033[92m{'Multiple .mp3 files'}\033[0m successfully!!!")
+                    logger.info("Set current mode to Multiple .mp3 files successfully!!!")
                 else:
-                    print(f"Set current mode to \033[92m{'One .mp3 file'}\033[0m successfully!!!")
+                    logger.info("Set current mode to One .mp3 files successfully!!!")
             case '/s':
                 playing = playing.toggle()
-                if playing == SoundState.PLAYING:
-                    print("\033[92mSound on successfully\033[0m")
-                else:
-                    print("\033[92mSound off successfully\033[0m")
+                sound_msg = "Sound on" if playing == SoundState.PLAYING else "Sound off"
+                logger.info(sound_msg)
             case _:
-                print("\033[91mInvalid command!\033[0m")
+                logger.warning(f"Invalid command: {cmd}")
     return 0
 
 def start() -> None:
@@ -72,7 +74,7 @@ def start() -> None:
             command(user_text)
             return start()
 
-        print(f"Your text: \033[92m{user_text}\033[0m")
+        logger.info(f"Input text: {user_text}")
         try:
             tts: gTTS = gTTS(text=user_text, lang=default_lang)
             index = 1
@@ -82,14 +84,14 @@ def start() -> None:
                 index += 1
             tts.save(f'audio_{index}.mp3')
             file_path = os.path.join(current_path, f'audio_{index}.mp3')
-            print("Loading...")
+            logger.info(f"Saved audio file: {file_path}")
             if playing == SoundState.PLAYING:
-                print("Playing...")
                 sound.play_sound(file_path)
+                logger.info(f"Played audio: {file_path}")
             print("\033[92mGenerating successfully!!!\033[0m")
             print(f"Your file path: {file_path}")
         except AssertionError as e:
-            print(f"\033[91mFile: Error ({e})\033[91m\033[0m")
+            logger.error(f"File generation error: {e}")
 
     elif mode == ModeState.MULTIPLE_MODE:
         file_path: str = input('Enter your .txt file path to generate audio: ').strip(" '\"")
@@ -97,27 +99,28 @@ def start() -> None:
             command(file_path)
             return start()
         if os.path.exists(file_path):
-            print(f"Your .txt file path: \033[92m{file_path}\033[0m")
+            logger.info(f"Input text file: {file_path}")
             name_file = os.path.splitext(os.path.basename(file_path))[0]
             with open(file_path, 'r', encoding='utf-8') as f:
                 for index, line in enumerate(f):
                     text = line.strip()
                     try:
                         tts = gTTS(text=text, lang=default_lang)  # remove line break character
-                        print(f"Generating file {index + 1}: \033[92m{text}\033[0m")
+                        logger.info(f"Generated: {name_file}_{index + 1}.mp3 | Text: {text}")
                         tts.save(f'{name_file}_{index + 1}.mp3')
                     except AssertionError as e:
-                        print(f"\033[91mGenerating file {index + 1}: Error ({e})\033[91m\033[0m")
+                        logger.error(f"Error generating {name_file}_{index + 1}.mp3: {e}")
             print("\033[92mGenerating successfully!!!\033[0m")
             print(f"Your all audio files are saved in: {os.getcwd()}")
         else:
-            print("\033[91mYou entered an invalid path or not exists!\033[91m\033[0m")
+            logger.warning(f"Invalid file path: {file_path}")
     print("\n")
     start()
 
 
 if __name__ == '__main__':
     intro.banner()
+    logger.info("Application started")
     print(f'[+] Current language: \033[92m{default_lang} ({dict_langs[default_lang]})\033[0m')
     print(f'[+] Current mode: \033[92m{"One .mp3 file" if mode == ModeState.DEFAULT_MODE else "Multiple .mp3 files"}\033[0m')
     print(f'[+] Current sound mode: \033[92m{"On" if playing == SoundState.PLAYING else "Off"}\033[0m')
